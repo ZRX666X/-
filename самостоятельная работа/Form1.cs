1bfda7729877eb9dfb8945794f412991326cff6a
ZRX666X <Purto.V@mail.ru>
2026-02-05 12:54:58 +0500

    Add files via upload

---
+++ самостоятельная работа/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace самостоятельная_работа
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            const double ratio = 2.5;
+            listBox1.Items.Add("Дьюмы | Сантиметры");
+            listBox1.Items.Add("------------------");
+            for (int incher = 1; incher <= 20; incher++)
+            {
+                double centimeters = incher * ratio;
+                listBox1.Items.Add($"{incher,5} | {centimeters,10}");
+            }
+        }
+    }
+}
